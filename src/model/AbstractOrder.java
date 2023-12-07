87a284eb270d79ca38086ee8e2f43b72c0762a0e
PenroseUCN <150657689@ucn.dk>
2023-12-07 10:18:48 +0100

    Update AbstractOrder.java

--- src/model/AbstractOrder.java
+++ src/model/AbstractOrder.java
@@ -1,5 +1,26 @@
 package model;
 
-public abstract class AbstractOrder {
+import java.time.LocalDateTime;
 
+
+/**
+ * the abstract order than all the different lending or sale types inherit from.
+ * 
+ * @author Rumu11
+ * @version 07/12/2023
+ */
+public abstract class AbstractOrder {
+	private int orderNo;
+	private LocalDateTime date; 
+	private Customer customer;
+	
+	public AbstractOrder(int orderNo, LocalDateTime date) {
+		this.orderNo = orderNo;
+		this.date = date;
+	}
+	
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	
 }
