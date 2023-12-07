c8de6fe491dce17bb80ad01b164cb956c70c6010
PTSDeSiqueira <10514401@ucn.dk>
2023-12-07 12:36:01 +0100

    Add code to CustomerCtrl and CustomerContainer
    Co-Authored-By: Majbritt20 <150657429+Majbritt20@users.noreply.github.com>

--- src/controller/CustomerCtrl.java
+++ src/controller/CustomerCtrl.java
@@ -1,5 +1,32 @@
 package controller;
 
-public class CustomerCtrl {
+import model.Customer;
+import model.CustomerContainer;
 
+/**
+ * This class represents customer controller which creates an instance of
+ * customer container and sends the commands to the container
+ * @author Patrick Thor Sorenson De Siqueira, Majbritt Kjeldgaard Harsfort
+ * @version 0.1.0
+ */
+
+public class CustomerCtrl {
+	private CustomerContainer customerContainer;
+	
+	/**
+	 * Constructor creates an instance of customer container
+	 */
+	public CustomerCtrl() {
+		this.customerContainer = CustomerContainer.getInstance();
+	}
+	
+	/**
+	 * This method is used to call a method from customer container
+	 * 
+	 * @param phone phone number of customer
+	 * @return object customer
+	 */
+	public Customer findCustomer(String phone) {
+		return customerContainer.findCustomer(phone);
+	}		
 }
--- src/model/CustomerContainer.java
+++ src/model/CustomerContainer.java
@@ -1,5 +1,65 @@
 package model;
 
-public class CustomerContainer {
+import java.util.ArrayList;
 
+/**
+ * This class represents a customer container which can store and search for
+ * the class Customer
+ * @author Patrick Thor Sorenson De Siqueira, Majbritt Kjeldgaard Harsfort
+ * @version 0.1.0
+ */
+
+public class CustomerContainer {
+	private ArrayList<Customer> customers;
+	private static CustomerContainer instance;
+	
+	/**
+	 * Constructor creates a list containing customers
+	 */
+	private CustomerContainer() {
+		customers = new ArrayList<>();
+	}
+	
+	/**
+	 * This method creates a instance if it does no exist
+	 * 
+	 * @return instance
+	 */
+	public static CustomerContainer getInstance() {
+		if (instance == null) {
+			instance = new CustomerContainer();
+		}
+		return instance;
+	}
+	
+	/**
+	 * This method is used to search for a customer inside the array list
+	 * by using customers phone number
+	 * 
+	 * @param phone phone number of customer
+	 * @return object customer
+	 */
+	public Customer findCustomer(String phone) {
+		Customer customer = null;
+		for (int i = 0; i < customers.size() && customer == null; i++)
+			if (customers.get(i).getPhone().equalsIgnoreCase(phone)) {
+				customer = customers.get(i);
+			}
+		return customer;
+	}
+	
+	/**
+	 * This method adds a customer to the ArrayList
+	 * @param customer is an object from the class Customer
+	 * @return <code>true</code> or <code>false</code>
+	 */
+	public boolean addCustomer(Customer customer) {
+		boolean res = false;
+		//TODO: make sure no customers have the same phone number.
+		if (customer != null) {
+			res = customers.add(customer);
+		}
+		return res;
+	}
+	
 }
