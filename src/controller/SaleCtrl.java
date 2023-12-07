d65ff1884d0598d17cf2c3af30283f76e17fa3d6
JonasGlintborg <10503086@ucn.dk>
2023-12-07 11:21:04 +0100

    Jonas
    laver SaleCtrl

--- src/controller/SaleCtrl.java
+++ src/controller/SaleCtrl.java
@@ -1,5 +1,40 @@
 package controller;
 
-public class SaleCtrl implements SaleCtrlIF {
+import model.AbstractProduct;
+import model.Customer;
+import model.OrderContainer;
+import model.Sale;
 
+/**
+ * @author Jonas og Jakob
+ */
+public class SaleCtrl implements SaleCtrlIF {
+	private OrderContainer orderContainer;
+	private ProductCtrl productCtrl;
+	private CustomerCtrl customerCtrl;
+	private Sale sale;
+	
+	public SaleCtrl() {
+	
+	}
+
+	public Customer setCustomer(String phone) {
+		//TODO
+		return null;
+	}
+	
+	public AbstractProduct addProduct(String barcode) {
+		//TODO
+		return null;
+	}
+	
+	public Sale makeSale() {
+		//TODO
+		return null;
+	}
+	
+	public Sale compSale(double payment) {
+		//TODO
+		return null;
+	}
 }
