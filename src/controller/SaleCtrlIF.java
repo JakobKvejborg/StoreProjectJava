04a784c4add0c648cd5b9691eddca8ad88911d43
PenroseUCN <150657689@ucn.dk>
2023-12-07 11:21:48 +0100

    created SaleCtrlIF

--- src/controller/SaleCtrlIF.java
+++ src/controller/SaleCtrlIF.java
@@ -1,5 +1,14 @@
 package controller;
 
+import model.Customer;
+import model.Sale;
+import model.SellableIF;
+
 public interface SaleCtrlIF {
 
+	public Customer setCustomer(String phone);
+	public SellableIF addProduct(String barcode);
+	public Sale makeSale();
+	public Sale completeSale(double payment);
+	
 }
