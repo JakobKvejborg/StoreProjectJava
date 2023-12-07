dc5f955ae50f12502f44738f06671da371d22b23
JonasGlintborg <10503086@ucn.dk>
2023-12-07 11:09:48 +0100

    Jonas
    Laver ProductCtrl, Customer, ProductContainer
    Fjerner TestDummy, JakobsKlasse

--- src/model/ProductContainer.java
+++ src/model/ProductContainer.java
@@ -1,5 +1,32 @@
 package model;
 
+import java.util.ArrayList;
+/**
+ * @author Jonas
+ */
 public class ProductContainer {
-
+	private static ProductContainer instance;
+	private ArrayList<AbstractProduct> products;
+	
+	public ProductContainer() {
+	
+	}
+	
+	public static ProductContainer getInstance() {
+		if(instance == null) {
+			instance = new ProductContainer();
+		}
+		return instance;
+	}
+	
+	public AbstractProduct findProduct(String barcode) {
+		//TODO
+		return null;
+	}
+	
+	public boolean addProduct(AbstractProduct product) {
+		//TODO
+		return true;
+	}
 }
+
