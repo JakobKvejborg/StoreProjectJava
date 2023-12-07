645ae1d25aebd584136182a7d20846992e369a32
PenroseUCN <150657689@ucn.dk>
2023-12-07 11:28:49 +0100

    add author to SaleCtrlIF

--- src/controller/SaleCtrlIF.java
+++ src/controller/SaleCtrlIF.java
@@ -4,6 +4,14 @@ import model.Customer;
 import model.Sale;
 import model.SellableIF;
 
+
+/**
+ * all the methods that are required for the SaleTUI to work,
+ * so mock SaleCtrl is possible to make.
+ * 
+ * @author Penrose
+ * @version 07/12/2023
+ */
 public interface SaleCtrlIF {
 
 	public Customer setCustomer(String phone);
