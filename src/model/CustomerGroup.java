744c67ea98304419248883ec76eaf2add152d34e
JonasGlintborg <10503086@ucn.dk>
2023-12-07 10:32:59 +0100

    Jonas
    laver Customer, AbstractPerson, CustomerGroup

---
+++ src/model/CustomerGroup.java
@@ -0,0 +1,18 @@
+package model;
+
+public class CustomerGroup {
+	private String name;
+	private String type;
+	private int maxDiscount;
+	
+	public CustomerGroup(String name, String type, int maxDiscount) {
+		this.name = name;
+		this.type = type;
+		this.maxDiscount = maxDiscount;
+	}
+
+	public int getMaxDiscount() {
+		return maxDiscount;
+	}
+	
+}
