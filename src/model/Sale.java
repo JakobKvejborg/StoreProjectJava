0349a7b6d451efba69364a4f588fbd707b6a9c32
JuliaRafnUCN <152272464+JuliaRafnUCN@users.noreply.github.com>
2023-12-08 11:18:46 +0100

    Update Sale.java
    update Sale

--- src/model/Sale.java
+++ src/model/Sale.java
@@ -9,20 +9,31 @@ import java.util.ArrayList;
  * 
  * @author Penrose
  * @version 07/12/2023
+ * @since 07/12/2023
  */
 public class Sale extends AbstractOrder{
 	
 	private ArrayList<SaleOrderLine> saleOrderLines; 
-	
+/**
+ * Represents a sale of a product
+ * @param orderNo	contains the number unique to a specific sale
+ * @param date		contains the date the sale was made
+ */
 	public Sale(int orderNo, LocalDateTime date) {
 		super(orderNo, date);
 		saleOrderLines = new ArrayList<>();
 	}
-	
+/**
+ * adds a saleOrderLine to the sale
+ * @param saleOrderLine
+ */
 	public void addSaleOrderLine(SaleOrderLine saleOrderLine) {
 		saleOrderLines.add(saleOrderLine);
 	}
-	
+/**
+ * Gets price of all products for a sale
+ * @return a double representing the total cost of a sale
+ */
 	public double getPrice(){
 		double total = 0.0d;
 		for(int i = 0; i < saleOrderLines.size(); i++) {
