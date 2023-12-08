d8f13a3de7f042f6b1f8502102976672f4bacba2
Majbritt20 <10510394@ucn.dk>
2023-12-08 13:47:57 +0100

    Create CustomerTest.java
    Making the Unit test for Customer

---
+++ src/unitTest/CustomerTest.java
@@ -0,0 +1,45 @@
+package unitTest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import model.Customer;
+import model.CustomerGroup;
+
+/**
+ * This class represents a test of Customer
+ * @author Majbritt Kjeldgaard Harsfort
+ * @version 0.1.0
+ */
+
+public class CustomerTest {
+	private Customer customer;
+	private CustomerGroup groupWithDiscount;
+	private CustomerGroup groupWithoutDiscount;
+	
+	@BeforeEach
+	public void setUp() {
+		customer = new Customer("John Doe","Gade 1","1234567890","john@example.dk");		
+		
+	groupWithDiscount = new CustomerGroup("default group", "private", 20);
+	
+	
+	groupWithoutDiscount = new CustomerGroup("default group number two", "public", 10);
+	
+	}
+	
+	@Test
+	public void testGetMaxDiscountWithDiscount() {
+		customer.setCustomerGroup(groupWithDiscount);
+		int maxDiscount = customer.getMaxDiscount();
+		Assertions.assertEquals(15,maxDiscount);	
+	}
+	
+	@Test
+	public void testGetMaxDiscountWithoutDiscount() {
+		customer.setCustomerGroup(groupWithoutDiscount);
+		int maxDiscount = customer.getMaxDiscount()	;
+		Assertions.assertEquals(0,maxDiscount);	
+		}
+}
