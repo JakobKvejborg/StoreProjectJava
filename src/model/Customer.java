7e32f6a9310db203bcd2130fd99a7f3b93db32d4
JakobKvejborgUcn <10513107@ucn.dk>
2023-12-08 09:42:20 +0100

    Jakob
    Adds javadoc to AbstractOrder, Customer, CustomerContainer

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -7,15 +7,34 @@ package model;
 public class Customer extends AbstractPerson{
 	private CustomerGroup customerGroup;
 	
+	/**
+     * Constructs a Customer object with the provided details.
+     *
+     * @param name    The name of the customer.
+     * @param address The address of the customer.
+     * @param phone   The phone number of the customer.
+     * @param email   The email address of the customer.
+     */
 	public Customer(String name, String address, String phone, String email) {
 		super(name, address, phone, email);
 		// TODO Auto-generated constructor stub
 	}
 
+	
+	/**
+     * Sets the customer group of this customer.
+     *
+     * @param customerGroup The CustomerGroup object representing the customer's group.
+     */
 	public void setCustomerGroup(CustomerGroup customerGroup) {
 		this.customerGroup = customerGroup;
 	}
 	
+	/**
+     * Retrieves the maximum discount applicable to this customer based on their group.
+     *
+     * @return The maximum discount available for this customer.
+     */
 	public int getMaxDiscount() {
 		return customerGroup.getMaxDiscount();
 		//TODO
