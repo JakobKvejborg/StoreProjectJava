118bb6ee12cab8e8d4d2bc231f67e186791b0539
JuliaRafnUCN <152272464+JuliaRafnUCN@users.noreply.github.com>
2023-12-08 10:48:18 +0100

    Update AbstractPerson.java
    update AbstractPerson

--- src/model/AbstractPerson.java
+++ src/model/AbstractPerson.java
@@ -3,20 +3,31 @@ package model;
 
 /**
  * @author first author Jonas
+ * @version 07/12/2023
+ * @since 07/12/2023
  */
 public abstract class AbstractPerson {
 	private String name;
 	private String address;
 	private String phone;
 	private String email;
-	
+/**
+ * 	
+ * @param name
+ * @param address
+ * @param phone
+ * @param email
+ */
 	public AbstractPerson(String name, String address, String phone, String email) {
 		this.name = name;
 		this.address = address;
 		this.phone = phone;
 		this.email = email;
 	}
-
+/**
+ * Gets a phone's number
+ * @return a string representing the phones number
+ */
 	public String getPhone() {
 		return phone;
 	}
