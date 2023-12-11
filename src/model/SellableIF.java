fccfadca0556f33304f97714511b1e9c2a570e2f
JuliaRafnUCN <152272464+JuliaRafnUCN@users.noreply.github.com>
2023-12-11 09:04:57 +0100

    Update SellableIF.java
    update SellableIF

--- src/model/SellableIF.java
+++ src/model/SellableIF.java
@@ -8,8 +8,14 @@ package model;
  * @version 07/12/2023
  */
 public interface SellableIF {
-
+	/**
+	 * it gets the price of a product
+	 * @return price in double
+	 */
 	double getPrice();
-	
+	/**
+	 * 
+	 * @return true if is only allowed to sell quantities of 1
+	 */
 	boolean isUnique();
 }
