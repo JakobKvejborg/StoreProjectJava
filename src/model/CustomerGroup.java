ed22e3217eca8cb0c97ea9fcac7efae29af34c2d
JuliaRafnUCN <152272464+JuliaRafnUCN@users.noreply.github.com>
2023-12-11 10:11:26 +0100

    Update CustomerGroup.java
    update CustomerGroup

--- src/model/CustomerGroup.java
+++ src/model/CustomerGroup.java
@@ -2,6 +2,7 @@ package model;
 
 
 /**
+ * creates a a customer group and manages its maximum discount
  * @author Jonas
  * 
  * @version 11/12/2023
@@ -11,13 +12,22 @@ public class CustomerGroup {
 	private String name;
 	private String type;
 	private double maxDiscount;
-	
+	/**
+	 * creates a customer group
+	 * @param name			name of the group in string
+	 * @param type			represents the type of group in string
+	 * @param maxDiscount	the maximum amount of discount represented in double
+	 */
 	public CustomerGroup(String name, String type, double maxDiscount) {
 		this.name = name;
 		this.type = type;
 		this.maxDiscount = maxDiscount;
 	}
-
+	/**
+	 * 
+	 * @return a double representing the maximum discount a costumer group can get
+	 * 0.2 = 20%
+	 */
 	public double getMaxDiscount() {
 		return maxDiscount;
 	}
