0b9da9ef17cf7ed2a05563e2f1c5ed27e0e94fc8
PTSDeSiqueira <10514401@ucn.dk>
2023-12-11 12:55:42 +0100

    Added code to SaleTest

--- src/unitTest/SaleTest.java
+++ src/unitTest/SaleTest.java
@@ -1,5 +1,37 @@
 package unitTest;
 
-public class SaleTest {
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import model.SaleOrderLine;
+import model.Sale;
+import model.ShelfProduct;
+
+/**
+ * This class represents a test of Sale.
+ * 
+ * @author Patrick Thor Sorenson De Siqueira
+ * 
+ * @version 11/12/2023
+ * @since 11/12/2023
+ */
+public class SaleTest {
+	private Sale sale;
+	private SaleOrderLine saleOrderLineTest;
+	
+	@BeforeEach
+	public void setUp() {
+		sale = new Sale(1, null);
+		saleOrderLineTest = new SaleOrderLine(new ShelfProduct("Oakwood planks", "Plank space", "087543", 9.99, 19.99, 10), 1);
+		sale.addSaleOrderLine(saleOrderLineTest);
+	}
+	
+	@Test
+	public void GetPriceTest() {
+		double expectedPrice = saleOrderLineTest.getQuantity() * saleOrderLineTest.getProduct().getPrice();
+		double actualPrice = sale.getPrice();
+		assertEquals(expectedPrice, actualPrice, 0.01);
+	}
 }
