80b21846476cc7e0c57904915766a367afdfffa2
JuliaRafnUCN <152272464+JuliaRafnUCN@users.noreply.github.com>
2023-12-12 11:21:07 +0100

    Update TemporalPriceListTest.java
    update TemporalPriceListTEst

--- src/unitTest/TemporalPriceListTest.java
+++ src/unitTest/TemporalPriceListTest.java
@@ -7,12 +7,15 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import model.TemporalPriceList;
 /**
  * @author Julia Rafn
  * @version¨12/12/2023
  * @since 12/12/2023
  */
 class TemporalPriceListTest {
+	private TemporalPriceList temporalPriceList;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -24,6 +27,7 @@ class TemporalPriceListTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
+		
 	}
 
 	@AfterEach
