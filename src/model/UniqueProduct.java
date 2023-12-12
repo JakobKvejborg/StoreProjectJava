b44488d0e965c2d5c925e5c300853584ac2e7589
Jonas Glintborg <10503086@ucn.dk>
2023-12-12 10:38:05 +0100

    UniqueProduct
    tilføjer paramter price og discount samt laver get/set price og set discount

--- src/model/UniqueProduct.java
+++ src/model/UniqueProduct.java
@@ -7,14 +7,30 @@ package model;
  */
 public class UniqueProduct extends AbstractProduct {
 	private String warrenty;
+	private double price;
+	private double discount;
 	
-	public UniqueProduct(String name, String descripton, String barcode, double purchasePrice, String warrenty) {
+	public UniqueProduct(String name, String descripton, String barcode, double purchasePrice, double price, double discount, String warrenty) {
 		super(name, descripton, barcode, purchasePrice);
 		this.warrenty = warrenty;
+		this.price = price;
+		this.discount = discount;
 	}
 
 	public void setWarrenty(String warrenty) {
 		this.warrenty = warrenty;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public void setDiscount(double discount) {
+		this.discount = discount;
 	}
 
 	public boolean isUnique() {
