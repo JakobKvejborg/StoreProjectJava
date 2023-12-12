d94bb758a2573d0ba4c298d94455e91fe3b5d7fa
JuliaRafnUCN <152272464+JuliaRafnUCN@users.noreply.github.com>
2023-12-12 10:13:56 +0100

    Create TemporalPriceList.java
    Created TemporalPriceList

---
+++ src/model/TemporalPriceList.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.util.ArrayList;
+
+/**
+ * @author Julia Rafn
+ * version 12/12/2023
+ * since 12/12/2023
+ */
+
+public class TemporalPriceList {
+
+	private ArrayList<TemporalPrice> prices;
+	
+	
+}
