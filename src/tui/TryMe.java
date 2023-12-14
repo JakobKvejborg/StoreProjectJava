d9d758ef55d5a42de72b8f48c2dcc17cbad0942d
PTSDeSiqueira <10514401@ucn.dk>
2023-12-14 10:15:39 +0100

    Added code to TryMe

--- src/tui/TryMe.java
+++ src/tui/TryMe.java
@@ -1,5 +1,42 @@
 package tui;
 
-public class TryMe {
+import java.time.LocalDateTime;
 
+import model.Customer;
+import model.CustomerContainer;
+	
+import model.ShelfProduct;
+import model.ProductContainer;
+
+/**
+ * This class is used to create test environment with dummy data.
+ * 
+ * @author Patrick Thor Sorenson De Siqueira
+ * @version 14/12/2023
+ * @since 14/12/2023
+ */
+public class TryMe {
+	private static CustomerContainer customerContainer;
+	private static ProductContainer productContainer;
+	private static OrderContainer orderContainer;
+	private static LeaseContainer leaseContainer;
+	
+	public static void createCustomers() {
+		customerContainer = CustomerContainer.getInstance();
+		Customer customer1 = new Customer("CustomerName1", "Address1", "11111111", "mail1@mail.dk");
+		Customer customer2 = new Customer("CustomerName2", "Address2", "22222222", "mail2@mail.dk");
+		
+		customerContainer.addCustomer(customer1);
+		customerContainer.addCustomer(customer2);
+	}
+	
+	public static void createProducts() {
+		productContainer = ProductContainer.getInstance();
+		ShelfProduct product1 = new ShelfProduct("ProductName1", "ProductDestription1", "123456", 9.99, 19.99, 0, LocalDateTime.now());
+		ShelfProduct product2 = new ShelfProduct("ProductName2", "ProductDestription2", "654321", 25.99, 35.99, 15, LocalDateTime.now());
+	
+		productContainer.addProduct(product1);
+		productContainer.addProduct(product2);
+	}
+	
 }
