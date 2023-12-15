0b6be328c0c11a69490e9dfed4db8694326d69f4
PTSDeSiqueira <10514401@ucn.dk>
2023-12-15 13:24:49 +0100

    Added JavaDoc to CustomerGroup and Customer

--- src/model/CustomerGroup.java
+++ src/model/CustomerGroup.java
@@ -3,10 +3,11 @@ package model;
 import java.time.LocalDateTime;
 
 /**
- * creates a a customer group and manages its maximum discount
+ * The <code>CustomerGroup<code> creates a customer group 
+ * and manages its maximum discount.
+ *
  * @author Jonas
- * 
- * @version 11/12/2023
+ * @version 15/12/2023
  * @since 07/12/2023
  */
 public class CustomerGroup {
@@ -14,11 +15,13 @@ public class CustomerGroup {
 	private String type;
 	private TemporalPriceList maxDiscount;
 	private static CustomerGroup defaultCustomerGroup;
+	
 	/**
-	 * creates a customer group
-	 * @param name			name of the group in string
-	 * @param type			represents the type of group in string
-	 * @param maxDiscount	the maximum amount of discount represented in double
+	 * Constructs a <code>CustomerGroup</code>.
+	 * 
+	 * @param name			The name associated with the group.
+	 * @param type			The type associated with the group.
+	 * @param maxDiscount	The maximum amount of discount associated with the group.
 	 */
 	public CustomerGroup(String name, String type, double maxDiscount, LocalDateTime date) {
 		this.name = name;
@@ -26,20 +29,25 @@ public class CustomerGroup {
 		this.maxDiscount = new TemporalPriceList();
 		this.maxDiscount.addPrice(maxDiscount, date);
 	}
+	
 	/**
-	 * 
-	 * @return a double representing the maximum discount a costumer group can get
+	 * This method gets <code>maxDiscount</code>.
+	 * @return a <code>double</code> representing the maximum discount a costumer group can get
 	 * 0.2 = 20%
 	 */
 	public double getMaxDiscount(LocalDateTime date) {
 		return maxDiscount.getPrice(date);
 	}
+	
+	/**
+	 * This method gets <code>defaultCustomerGroup</code>.
+	 * @return a <code>defaultCustomerGroup<code> representing <code>CustomerGroup</code>. 
+	 */
 	public static CustomerGroup getDefaultCustomerGroup() {
 		if(defaultCustomerGroup == null) {
 			defaultCustomerGroup = new CustomerGroup("Default Group", "Default", 0.2d, LocalDateTime.of(2020, 1,1,1,1));
 		}
-		return defaultCustomerGroup;
-		
+		return defaultCustomerGroup;	
 	}
 	
 }
